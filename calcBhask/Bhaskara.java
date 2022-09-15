1b8f36486a832e7d86c2d2833928125e5b538687
CauaH186 <88608913+CauaH186@users.noreply.github.com>
2022-09-15 14:30:27 -0300

    Add files via upload

---
+++ calcBhask/Bhaskara.java
@@ -0,0 +1,32 @@
+
+package calcBhask;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cauã
+ */
+public class Bhaskara {
+    public Double a,b,c,respos, resneg;
+    
+    public void raiz (){
+        Scanner calculo = new Scanner (System.in); 
+        System.out.println("Esse programa irá calcular a formula de bhaskara");
+        System.out.print("Digite o valor de A: ");
+        a = calculo.nextDouble();
+        System.out.print("Digite o valor de B: ");
+        b = calculo.nextDouble();
+        System.out.print("Digite o valor de C: ");
+        c = calculo.nextDouble();
+        
+        
+        respos = (-b + Math.sqrt((Math.pow(b, 2) - 4*a*c))) / 2;            
+        resneg = (-b - Math.sqrt((Math.pow(b, 2) - 4*a*c))) / 2;
+        
+        System.out.println("Os resultados do calculo são: " + respos + " e " + resneg);
+        
+        
+    }
+    
+}
