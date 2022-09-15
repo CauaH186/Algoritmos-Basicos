505c384dfbbf72cc0bc90ea4166cc2349df98d26
CauaH186 <88608913+CauaH186@users.noreply.github.com>
2022-09-15 14:19:57 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,12 @@
+
+package programa;
+public class Teste {
+    
+    
+    public static void main(String[] args) { 
+        Bhaskara calculo = new Bhaskara ();
+        calculo.raiz();
+        
+    }
+}
+
