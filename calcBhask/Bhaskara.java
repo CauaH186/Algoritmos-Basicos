163b2fe6c31e6210da7ac5f868439ecfbc6c48d1
CauaH186 <88608913+CauaH186@users.noreply.github.com>
2022-09-15 14:49:09 -0300

    Update Bhaskara.java

--- calcBhask/Bhaskara.java
+++ calcBhask/Bhaskara.java
@@ -8,11 +8,12 @@ import java.util.Scanner;
  * @author Cauã
  */
 public class Bhaskara {
+       
     public Double a,b,c,respos, resneg;
-    
+
     public void raiz (){
         Scanner calculo = new Scanner (System.in); 
-        System.out.println("Esse programa irá calcular a formula de bhaskara");
+        System.out.println("Esse programa irá calcular uma equação de 2º grau usando a formula de Bhaskara");
         System.out.print("Digite o valor de A: ");
         a = calculo.nextDouble();
         System.out.print("Digite o valor de B: ");
@@ -20,13 +21,16 @@ public class Bhaskara {
         System.out.print("Digite o valor de C: ");
         c = calculo.nextDouble();
         
-        
-        respos = (-b + Math.sqrt((Math.pow(b, 2) - 4*a*c))) / 2;            
-        resneg = (-b - Math.sqrt((Math.pow(b, 2) - 4*a*c))) / 2;
-        
-        System.out.println("Os resultados do calculo são: " + respos + " e " + resneg);
-        
-        
+        /*
+        Calculo da formula de Bhaskara
+        */    
+        respos = (-b + Math.sqrt((Math.pow(b, 2) - 4*a*c)));            
+        resneg = (-b - Math.sqrt((Math.pow(b, 2) - 4*a*c)));
+        if (Math.sqrt(Math.pow(b, 2) - 4 *a*c) < 0){
+            System.out.println("Os resultados do calculo são: " + respos + " e " + resneg);
     }
-    
+        else {
+            System.out.println("A equação de 2º grau não possui raízes reais");
+        }
+    }
 }
