9dbfe72c9170b61cef518b43ddfffaca86965508
Caua <caua186@gmail.com>
2022-09-18 12:34:29 -0300

    Separação das faculdades

---
+++ Nassau/calcBhask/Teste.java
@@ -0,0 +1,12 @@
+
+package calcBhask;
+public class Teste {
+    
+    
+    public static void main(String[] args) { 
+        Bhaskara calculo = new Bhaskara ();
+        calculo.raiz();
+        
+    }
+}
+
--- restDivCont/Restaurante.java
+++
@@ -1,9 +0,0 @@
-
-package restDivCont;
-
-class Restaurante {
-    public static void main(String[] args) {
-        DivCont div = new DivCont ();
-        div.divisao();
-    }
-}
