3d8d8644e815fd0be7be4d0ed810c55e5542919e
Cauã <88608913+CauaH186@users.noreply.github.com>
2022-10-14 21:01:40 -0300

    Add files via upload

---
+++ Exercicos/Salario.java
@@ -0,0 +1,28 @@
+
+package salario;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Salario {
+
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        int opc = 1;
+        int i = 0;
+        int novoSalario = 0;
+        while (opc != 0) {
+            ++i;
+            Scanner ler = new Scanner(System.in);
+            System.out.println("Deseja continuar 1 - Sim     0 - Nao");
+            opc = ler.nextInt();
+            System.out.print("Digite o salario do funcionario: ");
+            
+            int salario = ler.nextInt();
+             
+            novoSalario = (salario + novoSalario) / i;
+            System.out.println("A media salarial da empresa eh de: " + novoSalario);
+    }
+    }
+    
+}
