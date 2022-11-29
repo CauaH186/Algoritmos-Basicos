73fbdc3763eccd9ec5aeb8d9144e50c77aea86d9
Cauã <88608913+CauaH186@users.noreply.github.com>
2022-11-29 03:57:42 -0300

    Update ProjetoPOO.java

--- Nassau/Projeto POO/src/projeto/poo/ProjetoPOO.java
+++ Nassau/Projeto POO/src/projeto/poo/ProjetoPOO.java
@@ -1,54 +1,73 @@
 package projeto.poo;
 
+// Alunos:
+//Cauã Henrique - 01564863
+//Isabelly Luisy - 01565513
+//Cassiano Ferreira - 01566503
+
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
-public class ProjetoPOO {
+class ProjetoPOO {
+  static double v1, v2;
 
+  public static void valores() {
+    Scanner ler = new Scanner(System.in);
+    System.out.print("Digite o primeiro valor: ");
+    v1 = ler.nextDouble();
+    System.out.print("Digite o primeiro valor: ");
+    v2 = ler.nextDouble();
+  }
 
-    public static void main(String[] args) {
-        Escolha sel = new Escolha();
-        while (sel.selecao()){
-            sel.opc = JOptionPane.showConfirmDialog(null, "Deseja fazer uma operacão?");
-            if (sel.selecao()) {
-                sel.opcS = JOptionPane.showInputDialog(null, "Escolha a operação que deseja realizar\n1 Soma \n2 Subtração\n3 Divisão\n4 Multiplicação");
-                switch (sel.opcS) {
-                    case "1":
-                        Operacoes o1 = new Soma();
-                        o1.setN1(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do primeiro número")));
-                        o1.setN2(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do segundo número")));
-                        o1.calcular();
-                        JOptionPane.showMessageDialog(null, "O valor da sua soma é de:" + o1.resultado); 
-                    break;
-                    case "2":
-                        Operacoes o2 = new Subtracao();
-                        o2.setN1(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do primeiro número")));
-                        o2.setN2(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do segundo número")));
-                        o2.calcular();
-                        JOptionPane.showMessageDialog(null,"O valor da sua subtração é de:" + o2.resultado); 
-                    break;
-                    case "3":
-                        Operacoes o3 = new Divisao();
-                        o3.setN1(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do primeiro número")));
-                        o3.setN2(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do segundo número")));
-                        o3.calcular();
-                        JOptionPane.showMessageDialog(null,"O valor da sua divisão é de:" + o3.resultado); 
-                    break;
-                    case "4":
-                        Operacoes o4 = new Multiplicacao();
-                        o4.setN1(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do primeiro número")));
-                        o4.setN2(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do segundo número")));
-                        o4.calcular();
-                        JOptionPane.showMessageDialog(null,"O valor da sua multiplicação é de:" + o4.resultado); 
-                    break;
-                    default:
-                        JOptionPane.showMessageDialog(null, "Opcão invalida. Tente novamente");
-                }
-            } else {
-                JOptionPane.showMessageDialog(null, "Obrigado por usar!");
-            }
-            
-        }
-        
+  public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null,
+        "Alunos que fizeram: \nCauã Henrique - 01564863 \nIsabelly Luisy - 01565513 \nCassiano Ferreira - 01566503");
+    Scanner ler = new Scanner(System.in);
+    int opc = 0;
+    while (opc != 5) {
+      System.out.print("1 - Soma \n2 - Subtração\n3 - Divisão\n4 - Multiplicação\n5 - Sair \nEscolha sua opção: ");
+      opc = ler.nextInt();
+      System.out.println("");
+      switch (opc) {
+        case 1:
+          Operacoes soma = new Soma();
+          valores();
+          soma.setN1(v1);
+          soma.setN2(v2);
+          System.out.println("O resultado da operacao é de: " + soma.calcular() + "\n");
+          break;
+
+        case 2:
+          Operacoes sub = new Subtracao();
+          valores();
+          sub.setN1(v1);
+          sub.setN2(v2);
+          System.out.println("O resultado da operacao é de: " + sub.calcular() + "\n");
+          break;
+
+        case 3:
+          Operacoes div = new Divisao();
+          valores();
+          div.setN1(v1);
+          div.setN2(v2);
+          System.out.println("O resultado da operacao é de: " + div.calcular() + "\n");
+          break;
+
+        case 4:
+          Operacoes mult = new Multiplicacao();
+          valores();
+          mult.setN1(v1);
+          mult.setN2(v2);
+          System.out.println("O resultado da operacao é de: " + mult.calcular() + "\n");
+          break;
+
+        case 5:
+          System.out.println("Obrigado por usar");
+          break;
+
+        default:
+          System.out.println("Opcão invalida. Tente novamente");
+      }
     }
-    
+  }
 }
