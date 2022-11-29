2a7051e6fd79d8c9d3e24b6c73c329bf34e041dc
Cauã <88608913+CauaH186@users.noreply.github.com>
2022-11-29 04:23:41 -0300

    Update Subtracao.java

--- Nassau/Projeto POO/src/projeto/poo/Subtracao.java
+++ Nassau/Projeto POO/src/projeto/poo/Subtracao.java
@@ -1,10 +1,7 @@
-
 package projeto.poo;
-
-public class Subtracao extends Operacoes{
-    @Override
-    public double calcular(){
-        resultado = getN1() - getN2();
-        return resultado;
-    }
+public class Subtracao extends Operacoes {
+  @Override
+  public double calcular() {
+    return getN1() - getN2();
+  }
 }
